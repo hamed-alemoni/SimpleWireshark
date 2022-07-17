65287723950243a22b258485255b9e9f15975cce
hamed <hamed.alemonie@gmail.com>
2022-07-17 16:44:49 +0430

    add new variable to check our window is modified or not

--- controller/variables.py
+++ controller/variables.py
@@ -2,6 +2,7 @@
 check_thread = True
 # counting number of packets
 packets_number = 0
+previous_packets_number = 0
 
 
 def set_check_thread(value: bool):
@@ -22,3 +23,13 @@ def set_packets_number(value: int):
 def get_packet_number() -> int:
     global packets_number
     return packets_number
+
+
+def set_previous_packets_number(value: int):
+    global previous_packets_number
+    previous_packets_number = value
+
+
+def get_previous_packet_number() -> int:
+    global previous_packets_number
+    return previous_packets_number
