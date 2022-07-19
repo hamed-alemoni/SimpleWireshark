b82c62b1428d8ceed5fc454c91c85eba401663a0
hamed <hamed.alemonie@gmail.com>
2022-07-19 19:32:57 +0430

    fix a bug(for start the program in second time we have to click twice in this button)

--- controller/start_button_controller.py
+++ controller/start_button_controller.py
@@ -1,29 +1,22 @@
 from threading import Thread
 from .sniffing import sniffing
 from .table_widget_contoller import write_info_in_table_widget_format
-from .variables import set_check_thread, get_check_thread, set_packets_number, get_packet_number, set_previous_packets_number
-
-is_thread_created = False
-thread = None
+from .variables import set_check_thread, get_check_thread, set_packets_number, get_packet_number, \
+    set_previous_packets_number
 
 
 def run_sniffing_in_thread(status_label, table_widget):
     status_label_settings(status_label)
 
     # make thread for sniffing live in network and run it when user push start button
-    start_sniffing_thread(sniffing_loop, (table_widget,))
-
     set_check_thread(True)
+
+    start_sniffing_thread(sniffing_loop, (table_widget,))
 
 
 def start_sniffing_thread(target, args):
-    global thread, is_thread_created
-
-    if not is_thread_created:
-        thread = Thread(target=target, args=args)
-        thread.start()
-    else:
-        thread.start()
+    thread = Thread(target=target, args=args)
+    thread.start()
 
 
 def status_label_settings(status_label):
@@ -34,7 +27,6 @@ def status_label_settings(status_label):
 
 def sniffing_loop(table_widget):
     while get_check_thread():
-
         packets_number = get_packet_number()
         set_previous_packets_number(packets_number)
         # get one sniffed packet
